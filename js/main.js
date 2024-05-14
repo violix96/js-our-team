a619fb9aae014422df1d0a1b0e7bc414ecbdd9d9
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-14 16:12:36 +0200

    [UPD] MILLESTONE 0

--- js/main.js
+++ js/main.js
@@ -5,3 +5,44 @@
 
 console.log('ciao mondo');
 
+// creazione dell'array di oggetti con le informazioni fornite 
+
+const team = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-carrol-chief-editor.jpg"
+    },
+
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        nome: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+
+    {
+        name: "Barbara Ramos",
+        role: " Graphic Designer",
+        imgage: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+console.log(team);
